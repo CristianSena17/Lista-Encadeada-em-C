8b7f7d82ae9a3fabe91972aebce16f77b179e736
CristianSena <cristianfsena@gmail.com>
2023-05-28 00:58:20 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lista.h"
+
+
+int main(void){
+
+ Lista estoque;
+
+ new_lista(&estoque);
+
+ printf("Ola Mundo");
+
+
+  return 0;
+}
